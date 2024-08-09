81172d7413e26605d261248a8d71016bd795142f
dpente <darrylpentecost@gmail.com>
2024-08-10 11:33:39 +1200

    feat: page urls taken

--- backend/itjobscraper/itjobscraper/spiders/trademespider.py
+++ backend/itjobscraper/itjobscraper/spiders/trademespider.py
@@ -7,4 +7,18 @@ class TrademespiderSpider(scrapy.Spider):
     start_urls = ["https://www.trademe.co.nz/a/jobs/it"]
 
     def parse(self, response):
+        jobs = response.css('div.tm-search-results__listing')
+        jobs.pop(0)
+        jobs.pop(0)
+        for job in jobs:
+            yield{
+                'url': job.css('a').attrib['href']
+            }
         pass
+
+
+
+# jobs = response.css('div.tm-search-results__listing')
+# First 2 are not real
+# job = jobs[2]
+# url =  job.css('a').attrib['href']
