ee8818608867790174aae356f1bf8dc6e705939b
dpente <darrylpentecost@gmail.com>
2024-08-10 14:33:54 +1200

    feat: trademe page 1 data scraping

--- backend/itjobscraper/itjobscraper/spiders/trademespider.py
+++ backend/itjobscraper/itjobscraper/spiders/trademespider.py
@@ -19,23 +19,40 @@ class TrademespiderSpider(scrapy.Spider):
         jobs.pop(0)
         jobs.pop(0)
             
-        title = ''
+        #into each job
         for job in jobs:
+            relative_url = ''
             
-            if job.css('div.tm-jobs-search-card__title ::text').get() is None:
-                title = job.css('div.tm-promoted-listing-info__title ::text').get()
+            if job.css('a.tm-jobs-search-card__link ::attr(href)').get() is None:
+                relative_url = job.css('a.tm-promoted-listing-card__link.tm-promoted-listing-card__link--branded.o-card.ng-star-inserted ::attr(href)').get()
             else:
-                title = job.css('div.tm-jobs-search-card__title ::text').get()
+                relative_url = job.css('a.tm-jobs-search-card__link ::attr(href)').get()
                 
-            yield{
-                'title': title,
-                'url': job.css('a').attrib['href']
-            }
+            job_url = 'https://www.trademe.co.nz/a/' + relative_url
+            print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+            print(job_url)
+            yield response.follow(job_url, callback= self.parse_job_page) 
+        
+        #pagination
+        # next_page = response.xpath("/html/body/tm-root/div[1]/main/div/tm-jobs-search-results/div/div/div[3]/tm-flex-search-results/div/div[2]/tg-pagination/nav/ul/li[8]/tg-pagination-link/a/@href").get()
+        # if next_page is not None:
+        #     next_page_url = 'https://www.trademe.co.nz' + next_page
+        #     yield response.follow(next_page_url, callback= self.parse)
             
-        next_page = response.xpath("/html/body/tm-root/div[1]/main/div/tm-jobs-search-results/div/div/div[3]/tm-flex-search-results/div/div[2]/tg-pagination/nav/ul/li[8]/tg-pagination-link/a/@href").get()
-        if next_page is not None:
-            next_page_url = 'https://www.trademe.co.nz' + next_page
-            yield response.follow(next_page_url, callback= self.parse)
+    def parse_job_page(self, response):
+        yield{
+            'title' : response.css('.jb-listing__header-details h1::text').get(),
+            'description' : response.css('.tm-jobs-listing-body__item-content ::text').getall(),
+            'location' : response.xpath("/html/body/tm-root/div[1]/main/div/ng-component/div/div[4]/div/div[1]/div/tg-row/tg-col/tm-jobs-listing-body/tm-key-details-rack/tg-rack/tg-rack-item[1]/div/div/tg-rack-item-secondary/text()").get(),
+            'company' : response.css('h2.p-h3.jb-listing__company-name ::text').get(),
+            'type': response.xpath("/html/body/tm-root/div[1]/main/div/ng-component/div/div[4]/div/div[1]/div/tg-row/tg-col/tm-jobs-listing-body/tm-key-details-rack/tg-rack/tg-rack-item[2]/div/div/tg-rack-item-secondary/text()").get(),
+            'duration': response.xpath("/html/body/tm-root/div[1]/main/div/ng-component/div/div[4]/div/div[1]/div/tg-row/tg-col/tm-jobs-listing-body/tm-key-details-rack/tg-rack/tg-rack-item[3]/div/div/tg-rack-item-secondary/text()").get()
+        }
+            
+#  if job.css('div.tm-jobs-search-card__title ::text').get() is None:
+#                 title = job.css('div.tm-promoted-listing-info__title ::text').get()
+#             else:
+#                 title = job.css('div.tm-jobs-search-card__title ::text').get()
 
 
 # jobs = response.css('div.tm-search-results__listing')
