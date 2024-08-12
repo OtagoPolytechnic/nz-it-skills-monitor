6107d7b8786763f056ff4d2ce3291d2ee4191b45
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-08-12 13:56:15 +1200

    feat: database connection

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,28 @@
-from flask import Flask
+import os
+import psycopg2
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route('/test-db')
+def test_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return jsonify({"db_version": db_version})
