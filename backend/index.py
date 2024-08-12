09db732775fa3e3a22484f501c2f6fdffdd7d96f
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-08-12 14:57:18 +1200

    feat: sqlalchemy stuff

--- backend/index.py
+++ backend/index.py
@@ -1,28 +1,30 @@
 import os
-import psycopg2
-from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from model import init_app, db
+from model.job import JobSchema, Job
 
 load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+init_app(app)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-@app.route('/test-db')
-def test_db():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT version()')
-    db_version = cur.fetchone()
-    cur.close()
-    conn.close()
-    return jsonify({"db_version": db_version})
+@app.route('/db_version', methods=['GET'])
+def get_db_version():
+    try:
+        # Connect to the database
+        with db.engine.connect() as connection:
+            # Query for the database version
+            result = connection.execute("SELECT version();")
+            version = result.fetchone()[0]
+            return jsonify({"database_version": version})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
