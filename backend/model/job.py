b78505a0636823ad603a7dc13649fc694f05b915
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-08-29 14:21:00 +1200

    refactor: edit date field to use current date

--- backend/model/job.py
+++ backend/model/job.py
@@ -2,6 +2,7 @@ from . import db
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow_sqlalchemy.fields import Nested
+from datetime import date
 
 class Job(db.Model):
     __tablename__ = 'jobs'
@@ -13,9 +14,8 @@ class Job(db.Model):
     type = db.Column(db.String(50))
     duration = db.Column(db.String(50))
     company = db.Column(db.String(255))
-    date_list = db.Column(db.Date)
-    date_end = db.Column(db.Date)
-    skills = db.relationship('Skill', backref='job', lazy=True)
+    date = db.Column(db.Date, default=date.today)
+    skills = db.relationship('Skill', backref='job', lazy=True, cascade="all, delete-orphan")  # If a job is deleted, all of its skills will also be deleted
 
 class Skill(db.Model):
     __tablename__ = 'skills'
@@ -36,5 +36,3 @@ class JobSchema(SQLAlchemyAutoSchema):
 
     skills = Nested(SkillSchema, many=True)
     salary = fields.Integer(required=False)
-    date_list = fields.Date(required=False)
-    date_end = fields.Date(required=False)
