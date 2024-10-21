a69b65a81e0f062cb05527ee859bcf8e7137e52d
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-10-21 14:53:52 +1300

    feat: add login and protected admin route to nav bar

--- Frontend/src/utils/Navigation.tsx
+++ Frontend/src/utils/Navigation.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../Screens/HomeScreen"
 import Admin from "../Screens/AdminScreen";
+import ProtectedRoute from '../components/ProtectedRoute';
+import Login from '../Screens/LoginScreen';
 
 const Navigation = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+                    path="/admin"
+                    element={
+                        <ProtectedRoute>
+                            <Admin />
+                        </ProtectedRoute>
+                    }
+                />
       </Routes>
     </Router>
   );
