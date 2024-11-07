3771965aae7da63b30fbe48b40a4e9533a23185a
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-11-07 15:34:55 +1300

    refactor: typescript friendly changes

--- Frontend/src/Screens/AdminScreen.tsx
+++ Frontend/src/Screens/AdminScreen.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Admin = () => {
+const AdminScreen: React.FC = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
