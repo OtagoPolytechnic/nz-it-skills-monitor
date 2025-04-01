fb1a137b53c2ec0d022ae22cb4d34ebc33bd1623
SupaHotBall <lees27@student.op.ac.nz>
2025-04-01 22:36:42 +1300

    Feat: Added the logic for the ITJobsByCountry

---
+++ Frontend/src/components/ITJobsByCountryCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const cityData = [
+  { city: 'Auckland', percentage: 32 },
+  { city: 'Wellington', percentage: 21 },
+  { city: 'Christchurch', percentage: 15 },
+  { city: 'Queenstown', percentage: 8 },
+  { city: 'Rotorua', percentage: 7 },
+  { city: 'Dunedin', percentage: 5 },
+  { city: 'Tauranga', percentage: 4 },
+  { city: 'Napier', percentage: 3 },
+  { city: 'Hamilton', percentage: 3 },
+  { city: 'Nelson', percentage: 2 }
+];
+
+const ITJobsByCountryCard = () => {
+  return (
+    <div className="card">
+      <h3>IT Jobs by Country</h3>
+      <ul className="city-list">
+        {cityData.map((city, idx) => (
+          <li key={idx} className="city-item">
+            <span>{city.city}</span>
+            <span>{city.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ITJobsByCountryCard;
