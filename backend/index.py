8eaabffbdf0c51b82d53164a9958d1de61b30244
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2025-04-28 14:51:45 +1200

    fix: stoped the neon from idling out

--- backend/index.py
+++ backend/index.py
@@ -27,6 +27,10 @@ CORS(app)
 sock = Sock(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True
+}
+
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['ADMIN_USERNAME'] = os.getenv('ADMIN_USERNAME')
