65e891448ebed7475486dcd7f7e1cf8d5d41fec6
AhmadShoaib1 <nabias1@student.op.ac.nz>
2025-06-02 13:58:47 +1200

    fix: corrected dependencies for frontend, removed the unwanted in home.jsx and added the heatmap, staging runs fine now

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import ChartWrapper from "./ChartStyle/ChartWrapper";
-import ITJobsByCountryCard from './ITJobsByCountryCard';
 import SkillsChart from './SkillsChart';
 import LeafletHeatmap from './Heatmap';
 import "../App.css";
@@ -259,7 +258,7 @@ const Home = () => {
             )}
           </>
         )}
-        <ITJobsByCountryCard />
+        <LeafletHeatmap />
       </div>
     </div>
   );
