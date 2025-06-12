580a31b934762d89eac21a85f2643a95a4a3d678
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2025-06-12 14:40:38 +1200

    feat: add scripts to run spiders file

--- backend/seekscraper/run_spiders.py
+++ backend/seekscraper/run_spiders.py
@@ -5,3 +5,7 @@ import sys
 subprocess.run([sys.executable, '-m', 'scrapy', 'crawl', 'fetch_html'])
 
 subprocess.run([sys.executable, '-m', 'scrapy', 'crawl', 'job_spider'])
+
+subprocess.run([sys.executable, 'seekscraper/utils/process_openai_jobs.py'])
+
+subprocess.run([sys.executable, 'seekscraper/commit_openai_responses.py'])
