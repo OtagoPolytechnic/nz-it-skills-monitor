a6bca6f7bdb867b0bc5815f0cbe7dc01169f3b11
AhmadShoaib1 <nabias1@student.op.ac.nz>
2025-07-31 22:47:16 +1200

    changing the zoomablity of the map

--- Frontend/src/components/Heatmap.jsx
+++ Frontend/src/components/Heatmap.jsx
@@ -47,9 +47,9 @@ const LeafletHeatmap = () => {
     <div style={{ width: "100%", height: "100%", position: "relative", borderRadius: "8px", overflow: "hidden" }}>
       <MapContainer
         center={[-41.2865, 174.7762]}
-        zoom={6}
+        zoom={1}
         minZoom={5}
-        maxZoom={15}
+        maxZoom={10}
         maxBounds={[
           [-50, 165],
           [-33, 180],
