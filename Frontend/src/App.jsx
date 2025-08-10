377861aed99eca4185807957e02ea531fc15cbb1
SupaHotBall <lees27@student.op.ac.nz>
2025-08-10 17:28:18 +1200

    Fix: yea fixed 2 line of code which are import ITJobsScreen from './components/ITJobsScreen'; and <Route path=/it-jobs element={<ITJobsScreen />} />

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import ITJobsScreen from './components/ITJobsScreen';
 import AdminPage from './components/AdminPage';
 import './App.css';
 
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/it-jobs" element={<ITJobsScreen />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
