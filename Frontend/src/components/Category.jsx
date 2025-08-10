026b0c9898a778c8738aed04a1c9c9db70686793
SupaHotBall <lees27@student.op.ac.nz>
2025-08-10 17:11:03 +1200

    Feat: add Category dropdown with dynamic filtering in Home

--- Frontend/src/components/Category.jsx
+++ Frontend/src/components/Category.jsx
@@ -1,16 +1,16 @@
-// src/components/ITJobsScreen.jsx
+// src/components/Category.jsx
 import React from 'react';
 import Navbar from '../Navbar';
 
-const ITJobsScreen = () => {
+const Category = () => {
   return (
     <div>
       <Navbar />
       <div className="content-container">
-        <h2>IT Jobs Screen - Coming Soon!</h2>
+        <h2></h2>
       </div>
     </div>
   );
 };
 
-export default ITJobsScreen;
+export default Category;
