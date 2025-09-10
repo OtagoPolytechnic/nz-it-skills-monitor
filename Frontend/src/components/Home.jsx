8111ad98a34269d68d7a197f2b1bec36da1591f6
SupaHotBall <lees27@student.op.ac.nz>
2025-09-10 15:13:33 +1200

    decouple SalaryHistogram from filteredJobs

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -466,7 +466,7 @@ const fetchLocationSummary = async () => {
             }}
           >
             <h2 className="card-title">Salary Distribution</h2>
-            <SalaryHistogram jobs={filteredJobs} />
+            <SalaryHistogram/>
           </div>
         </div>
       </div>
